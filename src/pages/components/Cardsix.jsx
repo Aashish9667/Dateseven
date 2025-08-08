846b059aa3284d277dd847a2d0d410dc29d886d5
code with harry <aashish.kumar@96672gmail.com>
2025-08-08 11:33:17 +0530

    first name

---
+++ src/pages/components/Cardsix.jsx
@@ -0,0 +1,18 @@
+// import React from "react";
+// import styles from "./Animal.css";
+
+// export default function AnimalSlider() {
+//   return (
+//     <div className={styles.slider}>
+//       <div className={styles.slide}>
+//         <img src="https://placekitten.com/300/200" alt="Cat" />
+//       </div>
+//       <div className={styles.slide}>
+//         <img src="https://placedog.net/300/200" alt="Dog" />
+//       </div>
+//       <div className={styles.slide}>
+//         <img src="https://placebear.com/300/200" alt="Bear" />
+//       </div>
+//     </div>
+//   );
+// }
