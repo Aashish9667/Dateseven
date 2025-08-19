9ff4cf9ad6497f902db952c34694d5475c7549cf
code with harry <aashish.kumar@96672gmail.com>
2025-08-19 17:40:48 +0530

    updated navbar responsive

--- src/pages/components/Page1.jsx
+++ src/pages/components/Page1.jsx
@@ -16,6 +16,7 @@ export default function HeroSection() {
         my: { xs: 5, md: 12 }, // responsive margin (y-axis)
         textAlign: "center", // mobile में center align
         p: { xs: 2, md: 4 }, // padding adjust
+        marginTop:"100px"
       }}
     >
       {/* Main Heading */}
